01b5c07c1cd3437c82f70aeb3743231c73c9fc0e
Ryan Guthrie <ryanguthrie@zoho.com>
2021-10-10 21:41:29 +0100

    commit tree 88f44190fb6cd4b51e9b3a0f917802665e0f536d

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const splitString = word.split("")
+  const reverseArray = splitString.reverse()
+  const reverseWord = reverseArray.join("")
+  
+  if (word === reverseWord) {
+    return true
+  } else {
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  // splitString into Array
+  Reverse Arrayu
+  rejoin array into string
+
+  if (word === reverseWord) {
+    return true
+  } else{
+    return false
+  }
+
+
 */
 
 /*
   Add written explanation of your solution here
+  1) break (word) into an Array. Create a copy of that array, but reversed (look for a built in function), join back to string.  Write and If statement that compares word === reversed word and write a true/false statement.
+
 */
 
 // You can run `node index.js` to view these console logs
